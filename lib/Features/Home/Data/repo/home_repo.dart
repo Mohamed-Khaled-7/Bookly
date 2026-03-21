dd22dac5e2224f3cb62509583cd017527db91d33
rabh777 <prorabh777@gmail.com>
2026-03-21 10:31:29 +0200

    create repo and create errors folder

--- lib/Features/Home/Data/repo/home_repo.dart
+++ lib/Features/Home/Data/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:project/Features/Home/Data/models/book_model/book_model.dart';
+import 'package:project/core/errors/faliers.dart';
+
 abstract class HomeRepo {
-  void fetchVerticalBooks();
-  void fetchHorizontalList();
+  Future<Either<Faliers,List<BookModel>>> fetchVerticalBooks();
+  Future<Either<Faliers,List<BookModel>>> fetchHorizontalList();
 }
